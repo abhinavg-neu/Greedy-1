87b8c7dda9339769dc6349563583305b6b9907b8
Abhinav Gangadharan <144839406+abhinavg-neu@users.noreply.github.com>
2025-03-07 15:09:22 -0500

    Attempt at Jump Game

---
+++ JumpGame.java
@@ -0,0 +1,14 @@
+class Solution {
+    //Time Complexity:O(n)
+    //Space Complexity:O(1)
+    public boolean canJump(int[] nums) {
+        
+        int destination = nums.length-1;
+
+        for (int i =nums.length-2;i >=0;i--){
+            if (i+nums[i] >= destination)
+            destination = i;
+        }
+        return destination ==0;
+    }
+}
